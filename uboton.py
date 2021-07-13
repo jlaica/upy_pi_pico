7d977a2ea3091e67f50f5a3bf6e644b61f7c7c6c
José Laica <jose.laica@protonmail.com>
2021-07-13 17:07:29 -0500

    add ejmplo con pulsador

---
+++ uboton.py
@@ -0,0 +1,20 @@
+#Script para Micropython
+#Bucle infinito V1
+#Valido para:
+# - ESP8266
+# - ESP32
+# - ESP01
+# - Raspberry Pi Pico
+
+#Por José Luis Laica Cornejo
+#Gye - Ecu
+#2021
+
+from machine import Pin #Configuración de pines como entrada
+boton = Pin(2, Pin.IN)     
+
+print("Esperando por botón")
+while ( boton.value()== 0 ):
+    pass
+
+print("Boton fue presionado .. ")
